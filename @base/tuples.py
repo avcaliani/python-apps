26326fe7b82515657f0d7318b1d26ace2a7ba333
Anthony Vilarim Caliani <avcaliani.it@gmail.com>
2018-06-18 14:15:05 -0300

    Adding some python base files.

---
+++ @base/tuples.py
@@ -0,0 +1,8 @@
+tuple_one = ("Anthony", "Raquel", "Joe")
+tuple_two = (12, 13)
+
+print(tuple_one)  # ('Anthony', 'Raquel', 'Joe')
+print(tuple_one[0])  # Anthony
+print(tuple_one[0:2])  # ('Anthony', 'Raquel')
+
+print(tuple_one + tuple_two)  # ('Anthony', 'Raquel', 'Joe', 12, 13)
