d7d9f1291938ae8718a9956673b9293cc1e051cb
Anthony Caliani <avcaliani.it@gmail.com>
2018-07-01 16:37:40 -0300

    Adding Inheritance file.

---
+++ @base/inheritance.py
@@ -0,0 +1,48 @@
+"""
+@author     Anthony Vilarim Caliani
+@github     github.com/avcaliani
+"""
+
+class Parent:
+    def __init__(self):
+        print("Parent.__init__()")
+    
+    @staticmethod
+    def whoami():
+        print("The Boss!")
+
+    def hello(self):
+        print("Parent.hello()")
+    
+    def call_parent(self):
+        print("Parent.call_parent()")
+
+    def __del__(self):
+        print("Parent.__del__()")
+
+
+class Child(Parent):
+    def __init__(self):
+        print("Child.__init__()")
+
+    def hello(self):
+        super().hello()
+        print("Child.hello()")
+    
+    def __del__(self):
+        print("Child.__del__()")
+
+
+# Testing Parent
+Parent.whoami() # Static
+parent = Parent()
+parent.hello()
+
+print("-------------------------")
+
+Child.whoami() # Static
+child = Child()
+child.hello()
+child.call_parent()
+
+print("-------------------------")
