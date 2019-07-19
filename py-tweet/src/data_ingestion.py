3b03455560190b5cabfb87459d9cbb20be58c6bb
Anthony <avcaliani.it@gmail.com>
2019-07-18 21:38:14 -0300

    Working with data.

--- py-tweet/src/data_ingestion.py
+++ py-tweet/src/data_ingestion.py
@@ -1,7 +1,8 @@
 from os.path import dirname, realpath, join
+import re
 import json
 import pandas
-from util.log import info, error
+from util.log import info, error, debug
 __author__  = 'Anthony Vilarim Caliani'
 __contact__ = 'https://github.com/avcaliani'
 __license__ = 'MIT'
@@ -9,6 +10,7 @@ __license__ = 'MIT'
 
 # CONSTANTS
 DATA_PATH = join(dirname(realpath(__file__)),'../data/tweets.txt')
+LINK_REGEX = r'https?://[^\s<>"]+|www\.[^\s<>"]+'
 
 
 def parse_tweets(file_path):
@@ -20,6 +22,16 @@ def parse_tweets(file_path):
       error(f'Error while parsing data to JSON. Data: { line }')
   
   return data
+
+
+def word_in_text(word, text):
+  _word, _text = str(word).lower(), str(text).lower()
+  return True if re.search(_word, _text) else False
+
+
+def extract_link(text):
+  match = re.search(LINK_REGEX, str(text))
+  return match.group() if match else ''
 
 
 info("""
@@ -47,11 +59,70 @@ try:
     file_content
   ))
 
+  # Looking for tweets origin
   tweets_by_lang = tweets['lang'].value_counts()
-  info(f'TOP 5 LANGUAGES IN WHICH THE TWEETS WERE WRITTEN\n{tweets_by_lang[:5]}')
-  
   tweets_by_country = tweets['country'].value_counts()
-  info(f'TOP 5 COUNTRIES FROM WHICH THE TWEETS WERE SENT\n{tweets_by_country[:5]}')
-  
+
+  debug(tweets)
+  debug(f'"tweets" Data Frame Shape: { tweets.shape }')
+
+  info("=~= TOP 5 =~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=")
+  info(f'Languages In Which The Tweets Were Written\n{ tweets_by_lang[:5] }')
+  info(f'Countries From Which The Tweets Were Sent\n{ tweets_by_country[:5] }')
+  info(f'Available Tweets: { tweets.shape[0] }\n')
+
+
+  # Searching Python, Ruby, and Javascript related tweets
+  tweets['python'] = tweets['text'].apply(lambda tweet: word_in_text('python', tweet))
+  tweets['javascript'] = tweets['text'].apply(lambda tweet: word_in_text('javascript', tweet))
+  tweets['ruby'] = tweets['text'].apply(lambda tweet: word_in_text('ruby', tweet))
+
+  debug(tweets)
+  debug(f'"tweets" Data Frame Shape: { tweets.shape }')
+
+  info("=~= REFERENCES  =~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=")
+  info(f'* Python            { tweets["python"].value_counts()[True] }')
+  info(f'* JS                { tweets["javascript"].value_counts()[True] }')
+  info(f'* Ruby              { tweets["ruby"].value_counts()[True] }')
+  info(f'* Available Tweets  { tweets.shape[0] }\n')
+
+
+  # Searching relevant tweets
+  tweets['programming'] = tweets['text'].apply(lambda tweet: word_in_text('programming', tweet))
+  tweets['tutorial'] = tweets['text'].apply(lambda tweet: word_in_text('tutorial', tweet))
+  tweets['relevant'] = tweets['text'].apply(lambda tweet: word_in_text('programming', tweet) or word_in_text('tutorial', tweet))
+  relevant_tweets = tweets[tweets['relevant'] == True]
+
+  debug(tweets)
+  debug(f'"tweets" Data Frame Shape: { tweets.shape }')
+
+  info("=~= RELEVANT TWEETS =~=~=~=~=~=~=~=~=~=~=~=~=~=")
+  info(f'* Tweets about "programming"      { tweets["programming"].value_counts()[True] }')
+  info(f'* Tweets about "tutorial"         { tweets["tutorial"].value_counts()[True] }')
+  info(f'* --')
+  info(f'* Relevant Tweets                 { tweets["relevant"].value_counts()[True] }')
+  info(f'* Relevant Tweets about "Python"  { relevant_tweets["python"].value_counts()[True] }')
+  info(f'* Relevant Tweets about "JS"      { relevant_tweets["javascript"].value_counts()[True] }')
+  info(f'* Relevant Tweets about "Ruby"    { relevant_tweets["ruby"].value_counts()[True] }')
+  info(f'* Available Tweets                { tweets.shape[0] }\n')
+
+
+  # Searching relevant tweets with links
+  tweets['link'] = tweets['text'].apply(lambda tweet: extract_link(tweet))
+  relevant_tweets = tweets[tweets['relevant'] == True]
+  tweets_with_links = relevant_tweets[relevant_tweets['link'] != '']
+
+  debug(tweets)
+  debug(f'"tweets" Data Frame Shape: { tweets.shape }')
+
+  info("=~= RELEVANT TWEETS WITH LINK =~=~=~=~=~=~=~=~=")
+  info(f'* Relevant Tweets                 { relevant_tweets.shape[0] }')
+  info(f'* Relevant Tweets with Link       { tweets_with_links.shape[0] }')
+  info(f'* Relevant Tweets about "Python"  { tweets_with_links[ tweets_with_links["python"] == True ].shape[0] }')
+  info(f'* Relevant Tweets about "JS"      { tweets_with_links[ tweets_with_links["javascript"] == True ].shape[0] }')
+  info(f'* Relevant Tweets about "Ruby"    { tweets_with_links[ tweets_with_links["ruby"] == True ].shape[0] }')
+  info(f'* Available Tweets                { tweets.shape[0] }\n')
+
+
 except (KeyboardInterrupt, SystemExit):
   info('Bye bye!', True)
--- py-tweet/src/util/log.py
+++ py-tweet/src/util/log.py
@@ -1,9 +1,14 @@
 __author__  = 'Anthony Vilarim Caliani'
 __contact__ = 'https://github.com/avcaliani'
 __license__ = 'MIT'
-# Some Colors
+
+
+# CONSTANTS
+DEBUG_TAG = f'\033[1;35;40mDEBUG\033[0m'
 ERROR_TAG = f'\033[1;31;40mERROR\033[0m'
 INFO_TAG = f'\033[1;32;40mINFO\033[0m'
+DEBUG_ENABLED = True
+
 
 def info(data, no_tag=False):
   if no_tag:
@@ -11,5 +16,11 @@ def info(data, no_tag=False):
   else:
     print(f'{ INFO_TAG }  => { data }')
 
+
 def error(data):
   print(f'{ ERROR_TAG } => { data }')
+
+
+def debug(data):
+  if DEBUG_ENABLED:
+    print(f'{ DEBUG_TAG } => { data }')
