946dd24c2f5e7b8c8288144f862ea67a0494fc46
Anthony <avcaliani.it@gmail.com>
2019-10-11 23:15:18 -0300

    Adding CSV data to Cassandra

--- py-cassandra/main.py
+++ py-cassandra/main.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+import pandas as pd
 from cassandra.cluster import Cluster
 
 __author__ = 'Anthony Vilarim Caliani'
@@ -6,23 +6,44 @@ __contact__ = 'https://github.com/avcaliani'
 __license__ = 'MIT'
 
 
+def new_post(ss, row):
+    ss.execute("""
+    INSERT INTO posts (
+        id, authors, date, analyzed, category, content, img_src, section, tags, title, topics, url
+    ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+    """, (
+        str(row.id), str(row.authors), str(row.date), bool(row.analyzed), str(row.category), str(row.content),
+        str(row.img_src), str(row.section), str(row.tags), str(row.title), str(row.topics), str(row.url)
+    ))
+
+
+def new_author():
+    # TODO: Do it
+    pass
+
+
+def update_author():
+    # TODO: Do it
+    pass
+
+
 if __name__ == '__main__':
 
-    print(f'Connecting to our cluster...')
+    print(f'Connecting to Cassandra cluster...')
     cluster = Cluster(['0.0.0.0'], port=9042)
-    session = cluster.connect('py_data', wait_for_all_pools=True)
+    session = cluster.connect('py_cassandra_ks', wait_for_all_pools=True)
 
     # If you want to change the keyspace (It's not our case here)
     # session.set_keyspace('py_data')
 
-    print(f'Posts: {session.execute("SELECT COUNT(*) FROM posts").one()[0]}')
-    print(f'Inserting new post...')
-    session.execute(
-        "INSERT INTO posts (id, author, title, content, published_at) VALUES (%s, %s, %s, %s, %s)",
-        (4, 'isa', 'Post 04', 'Content 04', datetime.utcnow())
-    )
+    print(f'Reading data...')
+    posts = pd.read_csv('data/techcrunch-posts.csv')
+    posts['analyzed'] = False
+    print(f'"{posts.shape[0]}" posts found!')
 
-    print(f'(Updated) Posts: {session.execute("SELECT COUNT(*) FROM posts").one()[0]}')
-    rows = session.execute('SELECT * FROM posts')
-    for row in rows:
-        print(row.id, row.author, row.title, row.content, row.published_at)
+    print(f'Saving data to Cassandra database...')
+    for i, post in posts.iterrows():
+        new_post(session, post)
+
+    print(f'"{session.execute("SELECT COUNT(*) FROM posts").one()[0]}" posts saved!')
+    cluster.shutdown()
